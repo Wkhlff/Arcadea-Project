95bfd01bf0b63c8240f374b83663281f57efaffb
ZakiZakaria-006 <yomandiguna0306@gmail.com>
2026-04-29 13:09:04 +0700

    updated splashscreen

--- app/src/main/java/com/app/arcadeaproject/SignInActivity.kt
+++ app/src/main/java/com/app/arcadeaproject/SignInActivity.kt
@@ -6,9 +6,13 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install Splash Screen before super.onCreate and setContentView
+        installSplashScreen()
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
@@ -16,7 +20,6 @@ class SignInActivity : AppCompatActivity() {
         val tvGoToRegister = findViewById<TextView>(R.id.tv_go_to_register)
 
         btnSignIn.setOnClickListener {
-            // For now, any click will take the user to MainActivity
             Toast.makeText(this, "Welcome to Arcadea!", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
